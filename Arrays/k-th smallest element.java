369f1908d0d49bad18316405ea33bb953741040b
Riddhi Jain <jainriddhi2110@gmail.com>
2021-04-23 12:35:47 +0530

    Add files via upload

---
+++ Arrays/k-th smallest element.java
@@ -0,0 +1,42 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int t=sc.nextInt();
+		
+		while(t-->0)
+		{
+		    int n=sc.nextInt();
+		    
+		    int arr[]=new int[n];
+		    
+		    for(int i=0;i<n;i++)
+		    arr[i]=sc.nextInt();
+		    
+		    int k=sc.nextInt();
+		    Solution ob = new Solution();
+		    System.out.println(ob.kthSmallest(arr, 0, n-1, k));
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    public static int kthSmallest(int[] arr, int l, int r, int k) 
+    { 
+        //Your code here
+        Arrays.sort(arr);
+        return arr[k-1];
+        
+    } 
+}
