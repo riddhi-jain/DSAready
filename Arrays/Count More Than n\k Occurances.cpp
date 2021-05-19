17cbdfde3422bed683a8bbc40e1aece7e9ad1322
Riddhi Jain <jainriddhi2110@gmail.com>
2021-05-19 18:39:08 +0530

    Create Count More Than n\k Occurances.cpp

---
+++ Arrays/Count More Than n\k Occurances.cpp
@@ -0,0 +1,51 @@
+/*Given an array arr[] of size n and an element k. The task is to find the number of elements in array that appear more than n/k times.
+Approach:
+Create a hashmap and check for the value of each key in the map to be greater than n/k */
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    public:
+    map<int, int> m;
+    //Function to find all elements in array that appear more than n/k times.
+    int countOccurence(int arr[], int n, int k) {
+        int x = n/k;
+        int i;
+        int count =0;
+        for(i = 0; i< n; i++){
+            m[arr[i]]++;
+        }
+        for(auto i : m){
+            if(i.second > x){
+                count++;
+            }
+            else{
+                continue;
+            }
+        }
+        return count;
+    }
+};
+
+
+// { Driver Code Starts.
+int main() {
+    int t, k;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+
+        int arr[n];
+
+        for (i = 0; i < n; i++) cin >> arr[i];
+        cin >> k;
+        Solution obj;
+        cout << obj.countOccurence(arr, n, k) << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
