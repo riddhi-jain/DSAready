4648a98d104b585feee21ca6543dfb31528a443c
Riddhi Jain <jainriddhi2110@gmail.com>
2021-06-02 21:44:20 +0530

    Update LargestAreaofHistogram.cpp

--- Stacks & Queues/LargestAreaofHistogram.cpp
+++ Stacks & Queues/LargestAreaofHistogram.cpp
@@ -2,8 +2,8 @@
 //  Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the histogram.
 //  Input: heights = [2,1,5,6,2,3]
 //  Output: 10
-//  Explanation: The above is a histogram where width of each bar is 1.
-//  The largest rectangle is shown in the red area, which has an area = 10 units.
+//  Explanation: 
+
 //  Created by Giriraj Saigal on 28/05/21.
 //	Using Stack, Getting the smallest element from itself for every place from left and right.
 //	Using the width from left and right index to find largest area.
