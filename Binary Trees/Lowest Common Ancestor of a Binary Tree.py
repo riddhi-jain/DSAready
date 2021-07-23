d8dcbd5dc1cad6b1cc6ebf1568398cc6d537e423
Riddhi Jain <jainriddhi2110@gmail.com>
2021-07-23 15:58:58 +0530

    Update Lowest Common Ancestor of a Binary Tree.py

--- Binary Trees/Lowest Common Ancestor of a Binary Tree.py
+++ Binary Trees/Lowest Common Ancestor of a Binary Tree.py
@@ -12,7 +12,7 @@ Approach:
 1. Start traversing the tree from the root node.
 2. If the current node itself is one of p or q, we would mark a variable mid as True and continue the search for the other node in the left and right branches.
 3. If either of the left or the right branch returns True, this means one of the two nodes was found below.
-4. If at any point in the traversal, any two of the three flags left, right or mid become True, this means we have found the lowest common ancestor for the nodes p and q.
+4. If at any point in the traversal, any two of the three: flags left, right or mid become True, this means we have found the lowest common ancestor for the nodes p and q.
 
 Time complexity O(n) ans space complexity O(n)
 '''
