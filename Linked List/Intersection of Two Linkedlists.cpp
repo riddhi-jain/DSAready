74e2ab2ee38a8e5eb00686312c68a1ce43f791f7
Riddhi Jain <jainriddhi2110@gmail.com>
2021-09-05 12:16:22 +0530

    Create Intersection of Two Linkedlists.cpp

---
+++ Linked List/Intersection of Two Linkedlists.cpp
@@ -0,0 +1,131 @@
+/*Given two lists sorted in increasing order, create a new list representing the intersection of the two lists. The new list should be made with its own memory — the original lists should not be changed.
+
+Example 1:
+
+Input:
+L1 = 1->2->3->4->6
+L2 = 2->4->6->8
+Output: 2 4 6
+Explanation: For the given first two
+linked list, 2, 4 and 6 are the elements
+in the intersection.
+
+Approach:
+Two pointer Approach*/
+
+// { Driver Code Starts
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+Node* inputList(int size)
+{
+    Node *head, *tail;
+    int val;
+    
+    cin>>val;
+    head = tail = new Node(val);
+    
+    while(--size)
+    {
+        cin>>val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+}
+
+Node* findIntersection(Node* head1, Node* head2);
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,m;
+	    cin>> n >> m;
+	    
+	    Node* head1 = inputList(n);
+	    Node* head2 = inputList(m);
+	    
+	    Node* result = findIntersection(head1, head2);
+	    
+	    printList(result);
+	    cout<< endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
+
+
+/* The structure of the Linked list Node is as follows:
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+*/
+
+Node* findIntersection(Node* head1, Node* head2)
+{
+    // Your Code Here
+    Node* ptr1 = head1;
+    Node* ptr2 = head2;
+    Node* head = NULL;
+    Node* curr = NULL;
+    while(ptr1 and ptr2){
+        if(ptr1->data == ptr2->data){
+            if(head == NULL){
+                Node* temp = new Node(ptr1->data);
+                head = temp;
+                curr = temp;
+            }
+            else{
+                Node* temp = new Node(ptr1->data);
+                curr->next = temp;
+                curr = curr->next;
+            }
+            ptr1 = ptr1->next;
+            ptr2 = ptr2->next;
+        }
+        else{
+            if(ptr1->data < ptr2->data){
+                ptr1 = ptr1->next;
+            }
+            else{
+                ptr2 = ptr2->next;
+            }
+        }
+    }
+    return head;
+}
